939d56f56e0ae27ec75152a252c58a8af0573fb4
Md Kamrul Islam <kevin@wpmanageninja.com>
2025-06-16 00:18:30 +0600

    Update index.js

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,10 +1,13 @@
 // .vitepress/theme/index.js
 import DefaultTheme from 'vitepress/theme';
-import ChatbotWidget from './ChatbotWidget.vue'; // Direct sibling import
+import ChatbotWidget from './ChatbotWidget.vue'; // The component we're trying to integrate
 
 /** @type {import('vitepress').Theme} */
 export default {
-    extends: DefaultTheme,
+    extends: DefaultTheme, // Correctly extend the default theme
+
+    // This is the simplest way to override the layout to add a global component.
+    // We'll keep it here, as it's been tested.
     Layout: {
         extends: DefaultTheme.Layout,
         setup() { },
@@ -16,7 +19,7 @@ export default {
           </Layout>
         `,
         components: {
-            ChatbotWidget
+            ChatbotWidget // Register the component here
         }
     }
 };
