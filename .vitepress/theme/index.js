a4e01620bc1b9fc2c70f4d7d281258ceae998744
Md Kamrul Islam <kevin@wpmanageninja.com>
2025-06-15 23:58:24 +0600

    m

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,14 +1,13 @@
+// .vitepress/theme/index.js (or index.ts)
 import DefaultTheme from 'vitepress/theme';
-import ChatbotWidget from './ChatbotWidget.vue';
+import ChatbotWidget from './ChatbotWidget.vue'; // Direct sibling import
 
 /** @type {import('vitepress').Theme} */
 export default {
     extends: DefaultTheme,
     Layout: {
         extends: DefaultTheme.Layout,
-        setup() {
-            // This setup is for the Layout, not the component itself.
-        },
+        setup() { },
         template: `
           <Layout>
           <template #layout-bottom>
