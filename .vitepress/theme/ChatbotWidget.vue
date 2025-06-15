a369dce7a6798bd3807b5ccb25993deaf139db5d
Md Kamrul Islam <kevin@wpmanageninja.com>
2025-06-16 00:12:27 +0600

    MM

---
+++ .vitepress/theme/ChatbotWidget.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>Test Chatbot</div>
+</template>
+
+<script>
+export default {
+  name: 'TestChatbotWidget'
+}
+</script>
+
+<style>
+/* No scoped, no complex styles */
+div {
+  background-color: blue;
+  color: white;
+  padding: 10px;
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+}
+</style>
